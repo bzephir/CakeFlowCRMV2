830a5071336b4bde4be0609038d1f61bb9228bd7
Botija Rodriguez <botijarodriguez@gmail.com>
2025-06-15 00:09:35 -0400

    Updated CustomerDetail.tsx

--- src/pages/CustomerDetail.tsx
+++ src/pages/CustomerDetail.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {useEffect} from "react"
 import { useParams, useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import { 
