46ff8f8954305a07d512806a104598669ace9729
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-04 22:19:51 -0400

    Updated Inquiries.tsx

--- src/pages/Inquiries.tsx
+++ src/pages/Inquiries.tsx
@@ -290,7 +290,9 @@ const Inquiries: React.FC = () => {
                     <td className="px-6 py-4 whitespace-nowrap">
                       <div className="text-sm text-gray-900 capitalize"> {inquiry.type} </div>
                       <div className="text-sm text-gray-500">
-                       
+                        {inquiry.eventDate && (
+                      <div className=/*"flex items-center"*/  "text-sm text-gray-900 capitalize" >
+                          
                           </div>
                         )}
                        
