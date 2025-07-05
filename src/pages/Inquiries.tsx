4bc9cb8b063af70a0583afc6a896d67245fac4ee
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-04 21:52:40 -0400

    Updated Inquiries.tsx

--- src/pages/Inquiries.tsx
+++ src/pages/Inquiries.tsx
@@ -271,8 +271,7 @@ const Inquiries: React.FC = () => {
                       <div className="flex items-center">
                         <div className="flex-shrink-0">
                           <div className="w-8 h-8 bg-gradient-to-r from-coral-400 to-pink-400 rounded-full flex items-center justify-center">
-                           
-                          </div>
+                            <User className="h-4 w-4 text-white" />                          </div>
                         </div>
                         <div className="ml-3">
                           <div className="text-sm font-medium text-gray-900">
