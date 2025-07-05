5382ecc532f8d257e08d7e7ca38fc9fb677a119b
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-04 22:20:31 -0400

    Updated Inquiries.tsx

--- src/pages/Inquiries.tsx
+++ src/pages/Inquiries.tsx
@@ -298,6 +298,7 @@ const Inquiries: React.FC = () => {
                        
                       </div>
                     </td>
+                     {formatDate(inquiry.eventDate)}
                     <td className="px-6 py-4 whitespace-nowrap">
                       <span className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${getStatusColor(inquiry.status)}`}>
                         {inquiry.status}
