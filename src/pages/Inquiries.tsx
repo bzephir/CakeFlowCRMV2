eacbb4095f83cc366c2ed958608bd397a4ae4e62
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-04 22:03:25 -0400

    Updated Inquiries.tsx

--- src/pages/Inquiries.tsx
+++ src/pages/Inquiries.tsx
@@ -285,7 +285,7 @@ const Inquiries: React.FC = () => {
                       </div>
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm text-gray-900 capitalize">  {getTypeIcon(inquiry.type)}{inquiry.type} |  {formatDate(inquiry.eventDate)}</div>
+                      <div className="text-sm text-gray-900 capitalize"> {getTypeIcon(inquiry.type)}{inquiry.type} |{formatDate(inquiry.eventDate)}</div>
                       <div className="text-sm text-gray-500">
                         {inquiry.eventDate && (
                           <div className=/*"flex items-center"*/  "text-sm text-gray-900 capitalize" >
