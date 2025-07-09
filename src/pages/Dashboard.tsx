aab9989e1f6dd8e2b52c1c21f55a80d11daa1867
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-08 20:58:48 -0400

    Fix JSX syntax error in Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -228,7 +228,6 @@ const Dashboard: React.FC = () => {
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       Address
                     </th>
-                  </div>
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
