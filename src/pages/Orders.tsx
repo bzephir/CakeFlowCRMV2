6fedaf28821be26825a2d26fc0b71cd374583a58
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-10 12:58:20 -0400

    Updated Orders.tsx

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -432,7 +432,7 @@ const Orders: React.FC = () => {
                       <div className="text-sm text-gray-900">{formatDate(order.eventDate)}</div>
                     </td>
                     <td className="px-2 py-1 whitespace-nowrap">
-                      <div className="text-sm text-gray-900">{formatTIme(order.eventTime)}</div>
+                      <div className="text-sm text-gray-900">{formatTime(order.eventTime)}</div>
                     </td>
                     <td className="px-2 py-1 whitespace-nowrap text-right">
                       <div className="text-sm font-medium text-gray-900">{formatCurrency(order.total)}</div>
