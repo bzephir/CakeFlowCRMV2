e61040199b6b707f99c2eb7cc9b86180a02afb7b
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-10 14:41:23 -0400

    Updated EventModal.tsx

--- src/components/EventModal.tsx
+++ src/components/EventModal.tsx
@@ -214,6 +214,7 @@ const EventModal: React.FC<EventModalProps> = ({
           {/* Title */}
           <div>
             <label htmlFor="title" className="block text-sm font-medium text-gray-700 mb-1">
+              {formData.type === 'Title *'}
             </label>
             <input
               type="text"
