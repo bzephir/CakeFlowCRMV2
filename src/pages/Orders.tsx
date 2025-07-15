141fa5c4fab983ca9c90521a36da1af316c4f4fc
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-15 16:43:37 -0400

    Updated Orders.tsx

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -421,11 +421,10 @@ const Orders: React.FC = () => {
                     </td>
                     <td className="px-2 py-1 whitespace-nowrap">
                       <div className="text-sm text-gray-500">
-                        {inquiry.fulfillmentType === 'pickup' 
-                          ? `Pickup: ${inquiry.pickupTime ? formatTime(inquiry.pickupTime) : 'TBD'}`
-                          : `Delivery: ${inquiry.deliveryTime ? formatTime(inquiry.deliveryTime) : 'TBD'}`
+                        {order.fulfillmentType === 'pickup' 
+                          ? `Pickup: ${order.pickupTime ? formatTime(order.pickupTime) : 'TBD'}`
+                          : `Delivery: ${order.deliveryTime ? formatTime(order.deliveryTime) : 'TBD'}`
                         }
-                       
                       </div>
                     </td>
                     <td className="px-2 py-1 whitespace-nowrap text-right">
