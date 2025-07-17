ca9051ed9f4be90b4d74c06a02ccec96ad2dda50
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-17 15:23:35 -0400

    Updated Quotes.tsx

--- src/pages/Quotes.tsx
+++ src/pages/Quotes.tsx
@@ -307,7 +307,7 @@ const Quotes: React.FC = () => {
                       </div>
                     </td>
                     <td className="px-2 py-1 whitespace-nowrap">
-                      <div className="text-sm text-gray-900">{formatDate(quote.submittedAt)}</div>
+                      <div className="text-sm text-gray-900">{formatDate(quote.expirationDate)}</div>
                     </td>
                     <td className="px-2 py-1 whitespace-nowrap text-right">
                       <div className="text-sm font-medium text-gray-900">{formatCurrency(quote.total)}</div>
