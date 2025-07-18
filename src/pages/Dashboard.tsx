f3b9c47191f4cfcb1106b9e439ed5c7d88f3767a
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-18 17:07:55 -0400

    Updated Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -167,7 +167,7 @@ const Dashboard: React.FC = () => {
             icon={TrendingUp}
             color="pink"
           />
-        </div>
+        
          <div className="space-y-6">
           {/* Recent - Vertical List */}
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-8">
@@ -196,6 +196,7 @@ const Dashboard: React.FC = () => {
               </button>
             </div>
           </div>
+           </div>
 {/* Chart Placeholder Grid */}
 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
   <DashboardChartPlaceholder
