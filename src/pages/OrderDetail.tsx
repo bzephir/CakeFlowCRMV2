4d9eaea157dbe75ca70d1270aa20e284c1be225e
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-20 14:47:24 -0400

    Updated OrderDetail.tsx

--- src/pages/OrderDetail.tsx
+++ src/pages/OrderDetail.tsx
@@ -483,6 +483,10 @@ const OrderDetail: React.FC = () => {
                         <Calendar className="h-4 w-4 mr-2 text-gray-400" />
                         Event Type: {order.eventType}
                       </div>
+                       <div className="flex items-center text-sm text-gray-600">
+                        <Calendar className="h-4 w-4 mr-2 text-gray-400" />
+                        Event Date: {formatDate(quote.issueDate)} {/* Using issueDate as placeholder for eventDate */}
+                      </div>
                       {order.eventTime && (
                         <div className="flex items-center text-sm text-gray-600">
                           <Clock className="h-4 w-4 mr-2 text-gray-400" />
