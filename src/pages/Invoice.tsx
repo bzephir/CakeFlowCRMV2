547f12effa5cf261ec99010339d7c06dd2ab7748
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-20 15:32:57 -0400

    Updated Invoice.tsx

--- src/pages/Invoice.tsx
+++ src/pages/Invoice.tsx
@@ -466,7 +466,8 @@ const Invoice: React.FC = () => {
                         value={invoice.event.eventTime} 
                         onChange={(e) => handleInputChange(e, 'event', 'eventTime')}
                         className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-coral-500 focus:ring focus:ring-coral-500 focus:ring-opacity-50"/>
-                    </div>                    <div>
+                    </div>                    
+                    <div>
                       <label className="block text-sm font-medium text-gray-700">Fulfillment Type</label>
                       <select
                         value={invoice.event.fulfillmentType}
