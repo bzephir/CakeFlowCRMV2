615d67b9e4c77cb1b1e6529b940f9235a5d22c81
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-21 10:48:20 -0400

    Updated OrderDetail.tsx

--- src/pages/OrderDetail.tsx
+++ src/pages/OrderDetail.tsx
@@ -479,7 +479,7 @@ const OrderDetail: React.FC = () => {
                     <div className="space-y-2">
                       <div className="flex items-center text-sm text-gray-600">
                         <Calendar className="h-4 w-4 mr-2 text-gray-400" />
-                        Event Type: <span className="capitalize">order.eventType</span>
+                        Event Type: <div className="flex items-center text-sm text-gray-600 capitalize">{order.eventType)</div>
                       </div>
 
  <div className="flex items-center text-sm text-gray-600">
