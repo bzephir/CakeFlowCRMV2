7e8a9cbac42a1b3edc8129c11c2afad7d15af855
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-20 23:14:00 -0400

    Updated QuoteDetail.tsx

--- src/pages/QuoteDetail.tsx
+++ src/pages/QuoteDetail.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
-import { mockSampleQuote, mockSampleQuotesDetail } from '../data/mockData';
+import { MockQuoteDetail, mockSampleQuotesDetail } from '../data/mockData';
 import { formatDate, formatTime, formatCurrency } from '../utils/formatters';
 import { generateDocumentNumber } from '../utils/documentNumbering';
 import { 
