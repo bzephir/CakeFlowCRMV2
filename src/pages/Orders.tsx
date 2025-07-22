546deb36bd94405cb87ebf53ad10b1cd4cb3eb2b
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-21 21:00:14 -0400

    Updated Orders.tsx

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -223,31 +223,31 @@ const Orders: React.FC = () => {
                       />
                     </div>
                   </th>
-                  <th className="px-2 py-1 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">
+                  <th className="px-2 py-1 text-left text-xs font-bold text-gray-900 uppercase tracking-wider">
                     Order #
                   </th>
-                  <th className="px-2 py-1 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  <th className="px-2 py-1 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">
                     Customer
                   </th>
-                  <th className="px-2 py-1 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  <th className="px-2 py-1 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">
                     Event Type
                   </th>
-                  <th className="px-2 py-1 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  <th className="px-2 py-1 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">
                     Event Date
                   </th>
-                  <th className="px-2 py-1 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  <th className="px-2 py-1 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">
                     Fulfillment
                   </th>
-                  <th className="px-2 py-1 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  <th className="px-2 py-1 text-right text-xs font-bold text-gray-500 uppercase tracking-wider">
                     Total
                   </th>
-                  <th className="px-2 py-1 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  <th className="px-2 py-1 text-right text-xs font-bold text-gray-500 uppercase tracking-wider">
                     Balance
                   </th>
-                  <th className="px-2 py-1 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  <th className="px-2 py-1 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">
                     Status
                   </th>
-                  <th className="px-2 py-1 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  <th className="px-2 py-1 text-right text-xs font-bold text-gray-500 uppercase tracking-wider">
                     Actions
                   </th>
                 </tr>
