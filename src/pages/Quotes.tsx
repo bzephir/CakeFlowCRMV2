9ca57ba0b4da9f0c3a01684e19256bf78e4b8cb7
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-23 01:13:18 -0400

    Updated Quotes.tsx

--- src/pages/Quotes.tsx
+++ src/pages/Quotes.tsx
@@ -146,9 +146,11 @@ const Quotes: React.FC = () => {
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return (
-    <div className="flex-1 overflow-hidden">
-      <Header title="Quotes" />
-      
+      <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6 flex items-center gap-2">
+        <Receipt className="w-6 h-6 text-orange-500" />
+        Quotes
+      </h1>
       <div className="p-6">
         {/* Actions Bar */}
         <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-6">
