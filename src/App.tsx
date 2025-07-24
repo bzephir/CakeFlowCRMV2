29872b13807956eba6ff4137d7802940dc8cdbfc
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-24 00:48:15 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { InquiryProvider } from './context/InquiryContext';
 import Sidebar from './components/Sidebar';
 import Dashboard from './pages/Dashboard';
 import Orders from './pages/Orders';
