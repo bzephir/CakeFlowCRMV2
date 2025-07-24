ee3370fe9e1ec857fdcde5f8009ced9ee149bd61
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-24 16:20:20 -0400

    Updated InquiryContext.tsx

--- src/context/InquiryContext.tsx
+++ src/context/InquiryContext.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import { Inquiry, InquiryAction } from '../types';
 import { mockInquiries, getNewInquiriesCount } from '../data/mockData';
+import { InquiryProvider } from './context/InquiryContext';
 
 interface InquiryContextType {
   inquiries: Inquiry[];
