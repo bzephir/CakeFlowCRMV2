451069b54b4bdbe0fa06723fa66e82fab954de78
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-25 00:24:28 -0400

    Updated Quotes.tsx

--- src/pages/Quotes.tsx
+++ src/pages/Quotes.tsx
@@ -147,10 +147,7 @@ const Quotes: React.FC = () => {
 
   return (
       <div className="p-6">
-      <h1 className="text-2xl font-bold mb-6 flex items-center gap-2">
-        <Receipt className="w-6 h-6 text-orange-500" />
-        Quotes
-      </h1>
+ <Header title="Quotes" icon={Receipt} /> 
       <div className="p-6">
         {/* Actions Bar */}
         <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-6">
