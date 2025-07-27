88ba45f8502bc06029fb29be5f3955b30c057c3c
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-26 22:09:05 -0400

    Updated Forms.tsx

--- src/pages/Forms.tsx
+++ src/pages/Forms.tsx
@@ -52,7 +52,7 @@ const FormsModule: React.FC = () => {
               {Object.values(FormCategory).map((category) => (
                 <th
                   key={category}
-                  className="px-6 py-3 px-4 pt-4 pb-2 text-center text-xs font-medium text-gray-900 uppercase tracking-wider align-top bg-gradient-to-r from-coral-400 to-pink-400 rounded-t-lg border-t border-x border-coral-200 border-b-0 relative z-10 "
+                  className="px-6 py-3 px-4 pt-4 pb-4 text-center text-xs font-medium text-gray-900 uppercase tracking-wider align-top bg-gradient-to-r from-coral-400 to-pink-400 rounded-t-lg border-t border-x border-coral-200 border-b-0 relative z-10 "
                 >
                  
                 </th>
