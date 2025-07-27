a7add647fe65f5d0c0e7d1bee8abd57a1e24611d
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-27 19:53:49 -0400

    Updated Forms.tsx

--- src/pages/Forms.tsx
+++ src/pages/Forms.tsx
@@ -89,7 +89,7 @@ const FormsModule: React.FC = () => {
               setSelectedCategory(category);
               setSelectedFormId(null);
             }}
-            className={`text-lg font-semibold px-6 py-2 rounded-t-lg transition focus:outline-none ${
+            className={`text-md font-semibold px-6 py-2 rounded-t-lg transition focus:outline-none ${
               category === selectedCategory
                 ? "bg-coral-400 text-white shadow-md border border-b-transparent rounded-t-lg"
                 : "bg-gray-100 text-gray-600 hover:bg-coral-100 border border-transparent hover:border-coral-300"
