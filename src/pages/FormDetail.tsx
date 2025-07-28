7a164b6e2052bc642a18fdc2c77d930317a3582e
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-28 12:30:45 -0400

    Updated FormDetail.tsx

--- src/pages/FormDetail.tsx
+++ src/pages/FormDetail.tsx
@@ -74,7 +74,8 @@ const FormDetail: React.FC = () => {
    
   {/* Signature blocks */}
   <div className="mt-10">
-    <SignatureBlock role="Client" showAdminNote />
+    <SignatureBlock role="Client"/>
+    <SignatureBlock showAdminNote />
     <SignatureBlock role="Owner" />
   </div>
         </div>
