7aeefe257fbebcfd4e62714afdcf13d696c68fd7
Botija Rodriguez <botijarodriguez@gmail.com>
2025-07-28 11:53:36 -0400

    Updated FormDetail.tsx

--- src/pages/FormDetail.tsx
+++ src/pages/FormDetail.tsx
@@ -123,7 +123,7 @@ const FormDetail: React.FC = () => {
                 type="text"
                 name="ownerLastName"
                 placeholder="Enter last name"
-                className="border border-gray-300 rounded px-3 py-2"
+                className="border border-gray-300 rounded px-3 py-2 text-sm"
               />
             </label>
           </div>
