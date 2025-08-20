cd18ca9c05c272a286b50c87ab802b4a668d06e5
Botija Rodriguez <botijarodriguez@gmail.com>
2025-08-19 21:20:47 -0400

    Updated Forms.tsx

--- src/pages/Forms.tsx
+++ src/pages/Forms.tsx
@@ -86,7 +86,7 @@ const handleFormClick = (id: string) => {
   const maxFormsCount = Math.max(...categories.map(cat => templatesByCategory[cat].length));
 
   return (
-    <div className="mt-6">
+    <div className="p-6">
       <Header title="Forms" icon={FileSignature} />
 
       {/* Sorting Controls and New Form Button */}
