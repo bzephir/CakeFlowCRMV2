a6b9a7f1c4610f34092da06a36ce97cd654cce0a
Botija Rodriguez <botijarodriguez@gmail.com>
2025-08-19 22:23:19 -0400

    Fix missing JSX closing tag

--- src/pages/Forms.tsx
+++ src/pages/Forms.tsx
@@ -138,6 +138,10 @@ const handleFormClick = (id: string) => {
                   key={category}
                   className="px-6 py-2 text-left align-top font-semibold rounded-t-lg select-none"
                 >
+                  {category}
+                </th>
+              ))}
+            </tr>
           </thead>
           <tbody>
             {Array.from({ length: maxFormsCount }).map((_, rowIndex) => (
