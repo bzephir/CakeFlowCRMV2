d7f3acbca7cac9f553372f293c9fb9e0e2899de8
Botija Rodriguez <botijarodriguez@gmail.com>
2025-10-04 22:46:33 -0400

    Updated Packages.tsx

--- src/pages/Packages.tsx
+++ src/pages/Packages.tsx
@@ -3,6 +3,9 @@ import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import { Packages } from 'lucide-react';
 
+return ();
+  
+export default Packages ;
 <div className="p-6">
       <Header title="Packages" icon={ChefHat} />
 
